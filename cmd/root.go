03ab4ccf0973c966588cc8d47946b433d8b17c10
mdzobayer <mdzobayer@rocketmail.com>
2021-01-31 18:58:01 +0600

    root command rename

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "gaachwala",
+	Use:   "daakghar",
 	Short: "An e-commerce site for gacchwala.com",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -45,7 +45,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.yaml", "config file (default is $HOME/.gaachwala.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.yaml", "config file (default is $HOME/.daakghar.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
